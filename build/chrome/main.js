e1e97693389439e0cea2901373c0c4b5441b3622
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2013-07-15 15:52:37 +0300

    rebuilt bundles

--- build/chrome/main.js
+++ build/chrome/main.js
@@ -1,7 +1,3 @@
-var TEO = LOAD_BUNDLE(window);//load TEO bundle into global window object
-
-var os = require("os");
-
 TEO.on("loaded",function(){
     console.log(TEO);
     console.log("TEO Loaded.");
